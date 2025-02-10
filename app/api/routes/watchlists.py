5669bc01595983d9cc437cbce6025796d3f7a3f5
CShorter3 <CalidShorter3@gmail.com>
2025-02-10 17:55:28 -0500

    Imports flask's request library to watchlist routes file

--- app/api/routes/watchlists.py
+++ app/api/routes/watchlists.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, jsonify
 from flask_login import current_user, login_required
 from app.models import db, Watchlist, WatchlistStock
 
