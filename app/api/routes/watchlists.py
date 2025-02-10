d4e8830fb18f15d13053f8fe2eb9281e7baa1e98
CShorter3 <CalidShorter3@gmail.com>
2025-02-10 10:16:33 -0500

    Edits route comments to be more clear

--- app/api/routes/watchlists.py
+++ app/api/routes/watchlists.py
@@ -4,14 +4,14 @@ from app.models import db, Watchlist
 
 watchlists = Blueprint("watchlists", __name__)
 
-# GET watchlists
+# GET all watchlists
 @watchlists.route('/', methods=['GET'])
 @login_required
 def get_user_watchlists():
     watchlists = Watchlist.query.filter(user_id = current_user.id).all()
     return jsonify([watchlist.to_dict() for watchlist in watchlists])
 
-# DELETE watchlist
+# DELETE a watchlist
 @watchlists.route('/<int:id>', methods=['DELETE'])
 @login_required
 def delete_watchlist(id):
