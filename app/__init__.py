eedee2632163de7bbde5eb6dba7177c1d415d2ef
CShorter3 <CalidShorter3@gmail.com>
2025-02-09 22:56:08 -0500

    Registers watchlists route blueprint to init

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.routes.portfolio import portfolio
+from .api.routes.watchlists import watchlists
 from .seeds import seed_commands
 from .config import Config
 
@@ -30,6 +31,7 @@ app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(portfolio, url_prefix='/api/routes/portfolio')
+app.register_blueprint(watchlists, url_prefix='/api/routes/watchlists')
 db.init_app(app)
 Migrate(app, db)
 
