df32219c1363202339543c1b491cb841fd71d2ba
Calid <CalidShorter3@gmail.com>
2025-02-14 21:42:04 -0500

    Completed 1st draft of WatchlistPage and WatchlistStockTable without Redux state control

--- react-vite/src/components/WatchlistStockTable/WatchlistStockTable.jsx
+++ react-vite/src/components/WatchlistStockTable/WatchlistStockTable.jsx
@@ -1,46 +1,15 @@
 import { useEffect, useState } from "react";
 
-const WatchlistStockTable = ({ watchedStocks }) => {
+const WatchlistStockTable = ({ stockData }) => {
     console.log("*****INSIDE WATCHLIST STOCK TABLE!*****");
-
-    const [stockData, setStockData] = useState([]);
-
-    useEffect(() => {
-        const fetchStockData = async () => {
-            try {
-                const stockPromises = watchedStocks.map(async (symbol) => {
-                    const response = await fetch(
-                        `https://yahoo-finance15.p.rapidapi.com/api/v1/markets/quote?ticker=${symbol}&type=STOCKS`,
-                        {
-                            headers: {
-                                "x-rapidapi-host": "yahoo-finance15.p.rapidapi.com",
-                                "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY
-                            }
-                        }
-                    );
-                    const data = await response.json();
-                    console.log("Value of fetched data response: ", data)
-                    return data[0];
-                });
-
-
-                const stocks = await Promise.all(stockPromises);
-                setStockData(stocks);
-            } catch (error) {
-                console.error("Error fetching stock data:", error);
-            }
-        };
-
-        fetchStockData();
-    }, [watchedStocks]);
 
     return (
         <table>
             <thead>
                 <tr>
                     <th>Symbol</th>
-                    <th>Market Price</th>
-                    <th>Change %</th>
+                    <th>Price</th>
+                    <th>today</th>
                     <th>Market Cap</th>
                 </tr>
             </thead>
@@ -49,10 +18,8 @@ const WatchlistStockTable = ({ watchedStocks }) => {
                     <tr key={stock.symbol}>
                         <td>{stock.symbol}</td>
                         <td>${stock.regularMarketPrice.toFixed(2)}</td>
-                        <td style={{ color: stock.regularMarketChangePercent >= 0 ? "green" : "red" }}>
-                            {stock.regularMarketChangePercent.toFixed(2)}%
-                        </td>
-                        <td>${(stock.marketCap / 1e9).toFixed(1)}B</td>
+                        <td>{stock.regularMarketChangePercent.toFixed(2)}%</td>
+                        <td>${(stock.marketCap)}</td>
                     </tr>
                 ))}
             </tbody>
